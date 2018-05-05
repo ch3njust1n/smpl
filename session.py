3a06028ee44bd9b217cc32f61863451187208447
ch3njust1n <ch3njus@gmail.com>
2018-05-04 23:55:09 -0400

    Implemented features in issue #63

--- session.py
+++ session.py
@@ -12,6 +12,13 @@ import redis, ujson, argparse
 from pprint import pprint
 
 
+def get_object(var, cache):
+	if not cache.exists(var):
+		return 'key dne: {}'.format(var)
+	else:
+		return ujson.loads(cache.get(var))
+
+
 def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--host', type=str, default='localhost', help='Redis host')
@@ -23,41 +30,33 @@ def main():
 	parser.add_argument('--sess', '-s', type=str, help='Session objection id')
 	parser.add_argument('--property', '-p', type=str, help='Session object property')
 	parser.add_argument('--properties', '-ps', action='store_true', help='Get all properties of object')
+	parser.add_argument('--variable', '-v', type=str, help='Retrieve state variable. If using this, do not set --sess')
 	args = parser.parse_args()
 
 	cache = redis.StrictRedis(host=args.host, port=args.port, db=args.db)
-	ignore = ['parameters', 'gradients']
 	
 	if args.keys:
 		pprint('keys: {}'.format([key for key in cache.scan_iter("*")]))
 
-	if args.sess != None:
-		result = ''
+	sess = get_object(args.sess, cache) if args.sess != None else get_object(args.variable, cache)
 
-		if not cache.exists(args.sess):
-			result = 'dne sess_id: {}'.format(args.sess)
-		else:
-			sess = ujson.loads(cache.get(args.sess))
-			result = sess
-
-			if args.ignore != None:
-				tmp = dict(sess)
-				for k in args.ignore:
-					del tmp[k]
-				result = tmp
-			elif args.minimal:
-				tmp = dict(sess)
-				for k in ignore:
-					del tmp[k]
-				result = tmp
-			else:
-				if args.property != None:
-					value = sess[args.property]
-					result = 'key:{}, value: {}'.format(args.property, value)
-				if args.properties:
-					result = 'properties: {}'.format(list(sess.keys()))
+	if args.ignore != None:
+		for k in args.ignore:
+			if k in sess:
+				del sess[k]
+		pprint(sess)
+	elif args.minimal:
+		for k in ['parameters', 'gradients']:
+			if k in sess:
+				del sess[k]
+		pprint(sess)
+	else:
+		result = sess
+		if args.property != None:
+			result = 'key:{}, value: {}'.format(args.property, sess[args.property])
+		if args.properties:
+			result = 'properties: {}'.format(list(sess.keys()))
 		pprint(result)
-
 
 
 if __name__ == '__main__':
