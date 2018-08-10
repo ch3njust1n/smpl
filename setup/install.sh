055a971901492ded83fe159c4181b755b97e424b
Justin Chen <ch3njus@gmail.com>
2018-08-10 14:54:17 -0400

    Update install.sh

--- setup/install.sh
+++ setup/install.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
+# update system
 sudo apt-get update
+sudo apt-get upgrade
+
+# Install Anaconda and create env named smpl
+curl -O https://repo.continuum.io/archive/Anaconda2-5.0.1-Linux-x86_64.sh
+mv Anaconda2-5.0.1-Linux-x86_64.sh /tmp
+bash /tmp/Anaconda2-5.0.1-Linux-x86_64.sh
+conda create --name smpl python=2.7
+
+# Install dependencies
 while read requirement; do conda install --yes $requirement || pip install $requirement; done < requirements.txt
 sudo apt-get install redis-server
 conda install pytorch-cpu torchvision -c pytorch
 sudo apt install libgl1-mesa-glx
+
+# Make smpl.py executable
 chmod +x smpl.py
-redis-server --daemonize yes
+
+# Default activate Anaconda env
+echo "export PATH=\"/home/ubuntu/anaconda2/bin:$PATH\"" >> ~/.bashrc
+echo "source activate smpl" >> ~/.bashrc
+echo "redis-server --daemonize yes" >> ~/.bashrc
+
+source ~/.bashrc
