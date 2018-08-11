c6a884b99bf8dd37509485c58cb5ac41f3f3b17b
ch3njust1n <ch3njus@gmail.com>
2018-08-11 11:54:23 -0400

    Updated install to make smpl.py executable and removed that command from start.sh

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@ for server in $(cat server.txt)
 do
         printf "\n$server:\n"
         ssh -i ~/.ssh/smpl-0.pem "$server" "fuser -n tcp -k 9888; pkill -9 -f python;"
-        ssh -f -i ~/.ssh/smpl-0.pem "$server" "redis-server --daemonize yes; cd smpl; chmod +x smpl.py; ./smpl.py"
+        ssh -f -i ~/.ssh/smpl-0.pem "$server" "redis-server --daemonize yes; cd smpl; ./smpl.py"
 done
 ./smpl.py
